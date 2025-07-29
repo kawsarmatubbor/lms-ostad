18d2f38cf2f4bfc3f1155433eed2cd63b88e31a3
kawsarmatubbor <kawsarmatubbordec@gmail.com>
2025-07-29 07:29:50 +0600

    Registration view ok

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import serializers
+from . import models
 
-# Create your views here.
+class RegistrationSerializer(serializers.ModelSerializer):
+    password_2 = serializers.CharField(
+        write_only = True,
+        required = True
+    )
+    class Meta:
+        model = models.CustomUser
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password', 'password_2']
+
+    def create(self, validated_data):
+        validated_data.pop('password_2')
+        
+        user = self.Meta.model.objects.create_user(
+            username=validated_data['username'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            email=validated_data.get('email', ''),
+            password=validated_data['password'],
+        )
+        return user
+
+class RegistrationView(APIView):
+    def get(self, request):
+        return Response("Registration(GET)")
+    
+    def post(self, request):
+        serializer = RegistrationSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response("Registration successful")
+        
+        return Response(serializer.errors)
